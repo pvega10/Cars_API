a5ff95ff82fed7e580da642d1b4ec34f8afffbbf
Paul M Vega <Paul@macbook-pro-2.lan>
2022-06-23 21:34:16 -0500

    updated project urls, plus created first view function, plus created cars app urls file

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET' , 'POST'])
+
+def car_list(request):
+
+    return Response ("Hi Becka!")
